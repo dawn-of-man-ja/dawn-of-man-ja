ed0f490e9f6d57f6351ef6cfd14d69bd01950cff
tignear <tignear+m@gmail.com>
2019-03-05 00:23:02 +0900

    [release]文字化けを治す

--- workshop/deploy.ps1
+++ workshop/deploy.ps1
@@ -1,7 +1,6 @@
 $ErrorActionPreference="Stop"
 #APPVEYOR_REPO_COMMITの文字化け対策
-chcp 65001
-Set-Item env:LANG -Value ja_JP.UTF-8
+Set-Item env:LANG -Value ja_JP.sjis
 
 if(Test-Path Variable:env:changenote){
     $changenote=$env:changenote
