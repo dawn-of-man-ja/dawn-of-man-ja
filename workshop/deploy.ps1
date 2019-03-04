fc2f57eb817ba427a32bff464bf2303d2a8d668d
tignear <tignear+m@gmail.com>
2019-03-05 04:14:38 +0900

    [release]泥から抽出->泥を採取

--- workshop/deploy.ps1
+++ workshop/deploy.ps1
@@ -1,10 +1,11 @@
 $ErrorActionPreference="Stop"
-#APPVEYOR_REPO_COMMITの文字化け対策
 $env:LANG = "en_US.UTF-8";
 chcp 65001
 if(Test-Path Variable:env:changenote){
     $changenote=$env:changenote
 }else{
+    #APPVEYOR_REPO_COMMITの文字化け対策
+    #git logもいまいちうまく行かなかった。
     $query= "https://api.github.com/repos/$env:APPVEYOR_REPO_NAME/git/commits/$env:APPVEYOR_REPO_COMMIT";
     Write-Host "query:$query"
     $res=Invoke-RestMethod -Uri $query -Method GET
@@ -14,7 +15,6 @@ if(Test-Path Variable:env:changenote){
 
 Write-Host "changenote:$changenote"
 $content = $(Get-Content -Encoding UTF8 ".\workshop\scripts\workshop_item.vdf.template" ).Replace("<changenote>",$changenote) 
-#$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
 $scriptDir=Split-Path $myInvocation.MyCommand.Path -Parent 
 $path=[IO.Path]::Combine($scriptDir,".\workshop_item.vdf")
 Write-Host "path:$path"
