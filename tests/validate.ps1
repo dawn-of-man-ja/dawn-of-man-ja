0c9cb46fa68fd6e295a1f1bb18223509dafd4498
tignear <tignear+m@gmail.com>
2019-03-07 02:28:44 +0900

    fix test

--- tests/validate.ps1
+++ tests/validate.ps1
@@ -1,14 +1,15 @@
 $ErrorActionPreference="Stop"
 
-
+Write-Host "Starting test"
 Install-Module Pscx  -Scope CurrentUser -Force -AllowClobber
 
 
 $scriptDir=Split-Path $myInvocation.MyCommand.Path -Parent 
 $xmlPaths=[string[]](Join-Path $scriptDir "..\Strings\*.lng.xml" -Resolve)
 $xsd=Join-Path $scriptDir "lng.xsd"
-$ret=True
+$ret=$True
 Test-Xml -Path $xmlPaths -SchemaPath $xsd | ForEach-Object{$ret = $ret -And $_}
 if(!($ret)){
     exit 1#fail
 }
+Write-Host "Test OK"
