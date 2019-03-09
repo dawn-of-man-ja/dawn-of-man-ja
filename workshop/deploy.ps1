d3b4a1f0876f0cce7d4313bcd9850360151c8361
tignear <tignear+m@gmail.com>
2019-03-09 18:42:20 +0900

    stderrの内容を表示。

--- workshop/deploy.ps1
+++ workshop/deploy.ps1
@@ -1,4 +1,5 @@
 $ErrorActionPreference="Stop"
+$scriptDir=Split-Path $myInvocation.MyCommand.Path -Parent 
 $env:LANG = "en_US.UTF-8";
 chcp 65001
 #APPVEYOR_REPO_COMMITの文字化け対策
@@ -20,12 +21,15 @@ if(Test-Path Variable:env:changenote){
 
 Write-Host "changenote:$changenote"
 $content = $(Get-Content -Encoding UTF8 ".\workshop\scripts\workshop_item.vdf.template" ).Replace("<changenote>",$changenote) 
-$scriptDir=Split-Path $myInvocation.MyCommand.Path -Parent 
 $path=[IO.Path]::Combine($scriptDir,".\workshop_item.vdf")
 Write-Host "path:$path"
 
-    [IO.File]::WriteAllLines($path, $content)
+[IO.File]::WriteAllLines($path, $content)
 
-    Write-Host "content:`r`n$content"
+Write-Host "content:`r`n$content"
 
 .\workshop\bin\steamcmd.exe +login $env:steam_user $env:steam_password +workshop_build_item $path +quit
+
+$errPath=Join-Path $scriptDir ".\bin\logs\stderr.txt"
+Write-Host "errPath:$errPath"
+Get-Content $errPath
